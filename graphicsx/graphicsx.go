9fea0a4f51b85fe729b4a1f246d952e90aadafec
dwrolvink <dwrolvink@protonmail.com>
2020-06-17 09:34:40 +0200

    use renderer instead of screensurface

--- graphicsx/graphicsx.go
+++ graphicsx/graphicsx.go
@@ -27,15 +27,18 @@ import (
 
 type Graphics struct {
 	Window *sdl.Window
-	Screen *sdl.Surface
+	//Screen *sdl.Surface
+	Renderer *sdl.Renderer
 }
 
 // Add a function to the struct. We'll be able to call it like so:
 //		var graph = Graphics{window, screenSurface}
 //		graph.ClearScreen()
+/*
 func (this Graphics) ClearScreen() {  
     this.Screen.FillRect(nil, sdl.MapRGB(this.Screen.Format, 0xff, 0xff, 0xff));
 }
+*/
 
 // =====================================================================
 // 				Functions
@@ -44,8 +47,8 @@ func (this Graphics) ClearScreen() {
 // Loads SDL2
 // This function is used to:
 // - initialize SDL, and handle errors
-// - create a window, and get the screensurface from it
-// - return a struct containing the window and the screen
+// - create a window, and get the renderer from it
+// - return a struct containing the window and the renderer
 //
 // This struct can then be used to:
 // - draw on the window
@@ -73,7 +76,20 @@ func Initialize_graphics() Graphics {
 		os.Exit(2)
 	}
 
+	// Create a renderer. 
+	// A sdl.Renderer uses the hardware accelerated api available to you 
+	// (DirectX, OpenGL, OpenGL ES) and gives you a cross platform way to draw 
+	// graphical primitives (rectangles/lines/points) and images to the screen quickly.
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Failed to create renderer: %s\n", err)
+		os.Exit(2)
+	}
+	renderer.Clear()	
+
 	// window has been created, now need to get the window surface to draw on window
+	/*
+		// renderer is a more efficient replacement for surface
 	screenSurface, err := window.GetSurface()
 	if err != nil {
 		fmt.Fprint(os.Stderr, "Failed to create surface: %s\n", err)
@@ -81,4 +97,9 @@ func Initialize_graphics() Graphics {
 	}
 
 	return Graphics{window, screenSurface}
+	*/
+
+	return Graphics{window, renderer}
+
+	
 }
--- main.go
+++ main.go
@@ -24,34 +24,51 @@ import (
 func main() {
 	// Load SDL2, and get window and screen
 	graphics := graphicsx.Initialize_graphics()
-	var screenSurface = graphics.Screen
+	var renderer = graphics.Renderer
 	var window = graphics.Window
 
 	// Create grid of cells (just the rects for now)
 	var rect_grid = world.CreateRectGrid()
 
+	// Not sure if this line is strictly needed
+	var event sdl.Event
+
 	// Draw loop
 	var running = true
 	for running	{
 
-		/* Clear the entire screen to our selected color. */
-		graphics.ClearScreen()
+		/* Clear the entire screen to white. */
+		// set color white
+		// red, green, blue, alpha (alpha determines opaque-ness - usually 255)
+		renderer.SetDrawColor(255, 255, 255, 255)
+		// clear the window with specified color - in this case white.
+		renderer.Clear()
 
 		// Draw squares randomly from grid
 		var r_col = rand.Intn(64)
 		var r_row = rand.Intn(48)
-		screenSurface.FillRect(&rect_grid[r_row][r_col], sdl.MapRGB(screenSurface.Format, 0, 0, 0))
+		//screenSurface.FillRect(&rect_grid[r_row][r_col], sdl.MapRGB(screenSurface.Format, 0, 0, 0))
+		renderer.SetDrawColor(0, 0, 0, 255)
+		renderer.FillRect(&rect_grid[r_row][r_col])
 
 		// Draw Screen
-		window.UpdateSurface()
+		//window.UpdateSurface()
+			
+		// The rects have been drawn, now it is time to tell the renderer to show
+		// what has been draw to the screen
+		renderer.Present()
+
 
 		// Lag
 		time.Sleep(time.Millisecond * 1)
 
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		// handle events, in this case escape key and close window
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
+			// event that is sent when the window is closed
 			case *sdl.QuitEvent:
 				running = false
+			// keydown/keyup events
 			case *sdl.KeyboardEvent:
 				fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
 					t.Timestamp, t.Type, t.Keysym.Sym, t.Keysym.Mod, t.State, t.Repeat)
@@ -59,6 +76,7 @@ func main() {
 		}		
 	}
 	
+	renderer.Destroy()
 	window.Destroy()
 
 	// program is over, time to start shutting down. Keep in mind that sdl is written in C and does not have convenient
